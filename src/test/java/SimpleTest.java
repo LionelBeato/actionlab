a3676458b7d0f1088ebf9ed833218b8af3678dfb
Lionel Beato <lionel.beato@gmail.com>
2020-11-08 21:53:14 -0500

    Update SimpleTest.java

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -5,14 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class SimpleTest {
 
     @Test
-<<<<<<< HEAD:src/test/java/starter/SimpleTest.java
     public void myTest(){
         int expected = 7;
-=======
-    public void test(){
-        int expected = 5;
->>>>>>> 0f0f0028d96c07b2ff4c2938ac5c592267dfd36d:src/test/java/SimpleTest.java
-//        int actual = 3;
         int actual = 5;
         assertEquals(expected, actual);
     }
