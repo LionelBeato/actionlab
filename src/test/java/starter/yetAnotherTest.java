20b8a06b0eee259d2cd6b9f573415cfc259d4e09
Lionel Beato <lionel.beato@gmail.com>
2020-11-08 22:12:39 -0500

    add test

---
+++ src/test/java/starter/yetAnotherTest.java
@@ -0,0 +1,13 @@
+package starter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class yetAnotherTest {
+
+    @Test
+    public void otherTest(){
+        fail("wow!");
+    }
+}
