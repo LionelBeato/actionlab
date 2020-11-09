605c95a78b519d826d063b1af07df7d7743460a1
Lionel Beato <lionel.beato@gmail.com>
2020-11-08 21:20:23 -0500

    failing tests

--- src/test/java/starter/SimpleTest.java
+++ src/test/java/starter/SimpleTest.java
@@ -1,14 +1,23 @@
+package starter;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 
 public class SimpleTest {
 
     @Test
-    public void test(){
+    public void otherTest(){
+        fail("wow!");
+    }
+
+    @Test
+    public void myTest(){
         int expected = 5;
 //        int actual = 3;
-        int actual = 4;
+        int actual = 5;
         assertEquals(expected, actual);
     }
 
