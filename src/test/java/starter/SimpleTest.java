9a2be3132a666cbf5e75d77d53ec52cad6b3b49d
Lionel Beato <lionel.beato@gmail.com>
2020-11-08 21:53:55 -0500

    Update SimpleTest.java

--- src/test/java/starter/SimpleTest.java
+++ src/test/java/starter/SimpleTest.java
@@ -15,7 +15,7 @@ public class SimpleTest {
 
     @Test
     public void myTest(){
-        int expected = 8;
+        int expected = 7;
 //        int actual = 3;
         int actual = 5;
         assertEquals(expected, actual);
