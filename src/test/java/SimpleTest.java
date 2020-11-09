66690f5526e18b96aaa489af4112121ae21535ba
Lionel Beato <lionel.beato@gmail.com>
2020-11-08 20:17:43 -0500

    first commit

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleTest {
+
+    @Test
+    public void test(){
+        int expected = 5;
+//        int actual = 3;
+        int actual = 5;
+        assertEquals(expected, actual);
+    }
+
+}
